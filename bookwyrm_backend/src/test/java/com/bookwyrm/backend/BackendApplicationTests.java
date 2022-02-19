da5965656cb0806d33adbb8818e1e6aa1528e08f
Antony Anuraj <antony.anuraj@gmail.com>
2022-02-18 18:42:03 -0600

    Initialize Java Spring Project

---
+++ bookwyrm_backend/src/test/java/com/bookwyrm/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookwyrm.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
