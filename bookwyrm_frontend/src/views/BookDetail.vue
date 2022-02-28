4dee68962b3c9bc3c361421f3e3cedc7c7bb83fd
cjung <jungc@myumanitoba.ca>
2022-02-28 13:52:25 -0500

    Got flex Containers to work.

--- bookwyrm_frontend/src/views/BookDetail.vue
+++ bookwyrm_frontend/src/views/BookDetail.vue
@@ -1,32 +1,30 @@
 <template>
   <div class="book_detail">
     
-    <table id="enclosure">
-      <tr>
-        <td id="left_side">
-          
-          <div id="info_block">
-            <h2>{{book_title}}</h2>
-            <h5>By {{book_author}}</h5>
-          </div>
-          
-          <div id="description_block">
-            <p>{{book_description}}</p>
-          </div>
+    <div id="enclosure">
+      <div id="left_side">
+        
+        <div id="info_block">
+          <h2>{{book_name}}</h2>
+          <h5>By {{book_author}}</h5>
+        </div>
+        
+        <div id="description_block">
+          <p>{{book_description}}</p>
+        </div>
 
-          <div id="ratings_block">
-            <p>Ratings component goes here</p>
-          </div>
-          
-        </td>
+        <div id="ratings_block">
+          <p>Ratings component goes here</p>
+        </div>
+        
+      </div>
 
-        <td id="right_side">
-          <div id="reviews_block">
-            <p>Reviews component goes here</p>
-          </div>
-        </td>
-      </tr>
-    </table>
+      <div id="right_side">
+        <div id="reviews_block">
+          <p>Reviews component goes here</p>
+        </div>
+      </div>
+    </div>
     
   </div>
 </template>
@@ -37,7 +35,7 @@ export default {
   name: 'Book Detail',
   data(){
     return{
-      book_title:"Title",
+      book_name:"Title",
       book_author:"Author",
       book_description:"Book description goes here",
       book_ratings:{},
@@ -55,33 +53,42 @@ export default {
     width: 100%;
   }
   #enclosure{
-    border-collapse: separate;
-    display: block;
-    justify-content: space-between;
+    display: flex;
+    flex-direction: row;
+    flex-flow: row nowrap;
+    justify-content: stretch;
+    align-items: stretch;
   }
   #left_side{
-    display: inline-flex;
-    flex-grow: 1;
     flex-direction: column;
-    margin:15px;
+    flex-flow: column nowrap;
+    flex-grow: 1;
+    min-width: 25%;
+    max-width: 33%;
+    margin:10px;
   }
   #right_side{
-    display: inline-flex;
+    flex-direction: column;
+    flex-flow: column nowrap;
     flex-grow: 2;
-    flex-direction: row;
-    margin: 15px;
+    margin: 10px;
+    min-width: 50%;
+    max-width: 67%;
   }
   #info_block{
     display:flex;
     flex-direction: column;
+    flex-grow: 1;
   }
   #description_block{
     display:flex;
     flex-direction: column;
+    flex-grow: 4;
   }
   #ratings_block{
     display:flex;
     flex-direction: column;
+    flex-grow: 3;
   }
   #reviews_block{
     display: flex;
