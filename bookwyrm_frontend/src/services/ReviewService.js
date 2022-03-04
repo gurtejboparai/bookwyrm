0f7cd427e17046e4f760df8cb12dc7c3a17319de
CameronJung <98767840+CameronJung@users.noreply.github.com>
2022-03-04 18:49:07 -0500

    Update ReviewService.js
    Fixed my spelling of the word "anonymous".

--- bookwyrm_frontend/src/services/ReviewService.js
+++ bookwyrm_frontend/src/services/ReviewService.js
@@ -2,7 +2,7 @@ import { bookwyrmClient } from '@/client/AxiosClient';
 
 export default {
     
-    postReview(name, author, description, commentList, annonymous){
-        return bookwyrmClient.post('/api/review/',{name:name, author:author, description:description, commentList:commentList, annonymous:annonymous});
+    postReview(name, author, description, commentList, anonymous){
+        return bookwyrmClient.post('/api/review/',{name:name, author:author, description:description, commentList:commentList, anonymous:anonymous});
     }
 }
