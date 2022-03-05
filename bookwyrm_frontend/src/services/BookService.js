09e9b93e01c804e4a5bd0259d273c80859ac1fa5
Luke Morrow <41497984+LukeBMorrow@users.noreply.github.com>
2022-03-05 15:03:24 -0600

    #64 Add book search detail service

--- bookwyrm_frontend/src/services/BookService.js
+++ bookwyrm_frontend/src/services/BookService.js
@@ -3,5 +3,8 @@ import { bookwyrmClient } from '@/client/AxiosClient';
 export default {
   uploadBook(name, author){
     return bookwyrmClient.post('/api/book/',{name:name, author:author});
+  },
+  searchBookDetail(title){
+    return bookwyrmClient.get('/api/book/details/'+title);
   }
 }
