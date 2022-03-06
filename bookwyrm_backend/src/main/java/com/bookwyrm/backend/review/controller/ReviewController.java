0591ccf490384350ec26248570d9e42969c1753a
Luke Morrow <41497984+LukeBMorrow@users.noreply.github.com>
2022-03-05 20:53:06 -0600

    #72 Fix tests and Update Dao

--- bookwyrm_backend/src/main/java/com/bookwyrm/backend/review/controller/ReviewController.java
+++ bookwyrm_backend/src/main/java/com/bookwyrm/backend/review/controller/ReviewController.java
@@ -17,15 +17,6 @@ import java.util.List;
 public class ReviewController {
     ReviewService reviewService;
 
-    /*
-     * Requires some tests:
-     * Happy case (everything works as expected)
-     * Missing Author
-     * Missing Review ID
-     * Missing Review Description
-     * Missing Review Anonymous Flag
-     * Missing Everything
-     */
     @CrossOrigin
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ReviewUploadPayload> createReview(
@@ -36,13 +27,11 @@ public class ReviewController {
         HttpStatus status = HttpStatus.OK;
 
         if (errorList.isEmpty()) {
-            response.setReviewTitle(reviewUploadInput.getTitle());
-            response.setReviewAuthor(reviewUploadInput.getAuthor());
-            response.setReviewId(reviewUploadInput.getBookId());
-            response.setReviewContent(reviewUploadInput.getContent());
-            response.setReviewAnonymousFlag(reviewUploadInput.getAnonymousFlag());
-            ReviewDao reviewDao = new ReviewDao(reviewUploadInput.getAuthor(),
-                    reviewUploadInput.getTitle(), reviewUploadInput.getContent());
+            ReviewDao reviewDao = new ReviewDao(
+                    reviewUploadInput.getAuthor(),
+                    reviewUploadInput.getAnonymousFlag(),
+                    reviewUploadInput.getContent()
+            );
             reviewService.save(reviewDao);
         } else {
             response.setMessages(errorList);
