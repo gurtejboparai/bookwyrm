5f670dddf24d3e458d8906eca420aaed738e874e
Luke Morrow <41497984+LukeBMorrow@users.noreply.github.com>
2022-03-19 18:02:55 -0500

    #145 Update comment styling Fix bug in comment controller

--- bookwyrm_frontend/src/components/ReviewComponent.vue
+++ bookwyrm_frontend/src/components/ReviewComponent.vue
@@ -1,40 +1,42 @@
 <template>
     <div id="container">
-        <div id="focusedView" v-if="focused">
-            <div class="revDisplay">
-                <h5>{{topic.author}}</h5>
-                <p>{{topic.content}}</p>
-                <button v-on:click="hideDetail()">show all reviews</button>
-                <div>
-                    <div>
-                        <h5>Post a comment</h5>
-                        <form v-on:submit="postComment">
-                            <textarea name="commentInput" id="commentTextBox" v-model="newCommentText" rows="10" placeholder="Write your comment here"></textarea>
-                            <input type="submit" value="POST">
-                        </form>
-                    </div>
+        <div id="focusedView"  v-if="focused">
+            <button @click="hideDetail()" class="btn btn-light">show all reviews</button>
+            <div class=" p-3">
+                <div class="revDisplay card mt-4 p-2 shadow-sm">
+                    <h2>{{(topic.author == "" || topic.author == null) ? "- Guest -" : topic.author }}</h2>
+                    <p class="p-4">{{topic.content}}</p>
                 </div>
-                <div>
-                    <h4>Comments</h4>
-                    <div v-for="comment in topic.commentList" v-bind:key="comment.commentId">
+                <div class="pt-3">
+                    <h4 class="text-align-center">Comments</h4>
+                    <div class="card foreground p-3 shadow-sm m-3">
+                        <details>
+                            <summary>Add a comment</summary>
+                            <form @submit="postComment">
+                                <textarea name="commentInput" id="commentTextBox" v-model="newCommentText" placeholder="Write your comment here" class="rounded w-100 mt-3"></textarea>
+                                <input type="submit" class="btn btn-success mt-2">
+                            </form>
+                        </details>
+                    </div>
+                    <div v-for="comment in topic.commentList" :key="comment.commentId" class="pt-3 p-2">
                         <CommentComponent
-                            v-bind:commentId="comment.commentId"
-                            v-bind:author="comment.author"
-                            v-bind:content="comment.content"
-                            v-bind:anonymousFlag="comment.anonymousFlag"
+                            :commentId="comment.commentId"
+                            :author="comment.author"
+                            :content="comment.content"
+                            :anonymousFlag="comment.anonymousFlag"
                         />
                     </div>
                 </div>
             </div>
         </div>
         <div id="listView" v-else>
-            <div id="reviewCreator">
+            <div id="reviewCreator" class="card foreground p-3 shadow-sm">
                 <details>
                     <summary>Add a review</summary>
-                    <form v-on:submit="postReview">
+                    <form @submit="postReview">
                         <textarea name="reviewInput" id="reviewTextBox" class="rounded w-100 mt-3" 
                             placeholder="Write your review here" v-model="newReviewText"></textarea>
-                        <input type="submit" class="btn btn-success mt-2" value="POST">
+                        <input type="submit" class="btn btn-success mt-2">
                     </form>
                 </details>
             </div>
@@ -46,11 +48,6 @@
         </div>
     </div>
 </template>
-
-
-
-
-
 <script>
 import CommentComponent from "./CommentComponent.vue"
 export default{
@@ -98,19 +95,11 @@ export default{
         background-color: #3a3e41;
     }
     #commentTextBox{
-        display: block;
-        text-justify: start;
-        width:100%;
-        max-height: 4em;
-    }
-    #reviewCreator{
-        background-color: #34383a;
-        padding: 5px;
-        margin: 5px;
-        border-radius: 5px;
+        max-height: 5em;
+        min-height: 2em;
     }
     #reviewTextBox{
-        max-height: 15em;
+        max-height: 20em;
         min-height: 2em;
     }
 </style>
