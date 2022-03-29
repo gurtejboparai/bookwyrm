a4acd125ffaecab107199d6818732ef095a7e4a6
cjung <jungc@myumanitoba.ca>
2022-03-29 17:41:11 -0500

    Started work on a test for the review component - It isn't ready yet, I keep getting an error I can't explain - the error is: InvalidCharacterError: "0"  did not match the Name production: Unexpected syntax in top

---
+++ bookwyrm_frontend/tests/unit/components/RatingComponent.spec.js
@@ -0,0 +1,28 @@
+import {shallowMount} from '@vue/test-utils'
+import RatingComponent from '@/components/RatingComponent.vue'
+
+describe('RatingComponent.vue Test', ()=> {
+
+    it('renders the component', ()=> {
+        const wrapper = shallowMount(RatingComponent, {
+            props:[
+                'localRatings',
+                'nextNewRatingId'
+            ]
+        })
+        expect(wrapper.vm.$options.name).toMatch('RatingComponent')
+    })
+    it('processes valid prop data', ()=> {
+        const wrapper = shallowMount(RatingComponent, {
+            propsData:{
+                'localRatings': [],
+                'nextNewRatingId': 0
+            }
+        })
+
+        wrapper.setData({localRatings: [{ratingId:0, genre:'Gothic', score:2.5}]})
+        wrapper.setData({nextNewRatingId: 1})
+        expect(wrapper.vm.nextNewRatingId).toEqual(1)
+        expect(wrapper.vm.localRatings[0]).toEqual({ratingId:0, genre:'Gothic', score:2.5})
+    })
+})
