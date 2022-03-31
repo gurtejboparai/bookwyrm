81f67f22fbe1ca63badac75d03b722d7229f2a5c
Luke Morrow <41497984+LukeBMorrow@users.noreply.github.com>
2022-03-30 20:21:37 -0500

    #137 Remove imports that were causing build to fail

--- bookwyrm_backend/src/test/java/com/bookwyrm/backend/unit/book/controller/BookControllerTests.java
+++ bookwyrm_backend/src/test/java/com/bookwyrm/backend/unit/book/controller/BookControllerTests.java
@@ -11,18 +11,14 @@ import com.bookwyrm.backend.book.service.BookService;
 import com.bookwyrm.backend.comment.dao.CommentService;
 import com.bookwyrm.backend.review.dao.ReviewService;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.Assert;
 import org.springframework.web.client.RestTemplate;
 
-import java.awt.print.Book;
 import java.util.Arrays;
 
 import static org.mockito.ArgumentMatchers.any;
